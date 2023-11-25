24651c51e974e62a8b614f2cde0cdf098f9c8fbc
Mario <mceseahurtado00@mylangara.ca>
2023-11-24 22:47:14 -0800

    feat: Adding the hero section

--- src/app/components/Search.jsx
+++ src/app/components/Search.jsx
@@ -3,6 +3,8 @@
 // search context
 import { useContext } from "react";
 import { SearchContext } from "../context/search";
+import DateSelection from "./DateSelection";
+import HoursSelection from "./HoursSelection";
 
 // components
 import LocationSelection from "./LocationSelection";
@@ -17,8 +19,22 @@ const Search = () => {
           : "bg-white rounded-[20px] py-6 xl:pr-4 xl:h-[98px]"
       } hidden xl:block w-full relative shadow-lg`}
     >
-      <div className="xl:h-full flex items-center px-6 xl:px-0 ">
+      <div className={`flex h-full ${searchActive && "container mx-auto"}`}>
         <LocationSelection />
+        <DateSelection />
+        <HoursSelection />
+        {/* btn */}
+        <div className="xl:h-full flex items-center px-6 xl:px-0">
+          <button
+            className={`${
+              searchActive
+                ? "btn btn-sm btn-accent xl:w-[164px]"
+                : "btn btn-lg btn-accent xl:w-[184px]"
+            }`}
+          >
+            Search
+          </button>
+        </div>
       </div>
     </div>
   );
