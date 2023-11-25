40a820f0c5a681a61ff3d91e97a51cbd25b7a0bc
Mario <mceseahurtado00@mylangara.ca>
2023-11-25 00:25:47 -0800

    feat: Adding the Cars Section

--- src/app/components/Cars.jsx
+++ src/app/components/Cars.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
+// components
+import Brands from "./Brands";
+import CarSlider from "./CarSlider";
+
 const Cars = () => {
   return (
-    <section className="h-screen flex items-center bg-yellow-100" id="cars">
-      Cars
+    <section className="h-screen flex items-center" id="cars">
+      <div className="container mx-auto">
+        <Brands />
+        <CarSlider />
+      </div>
     </section>
   );
 };
